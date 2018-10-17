656a8cfa4ff4454e7332ca87da2039f6c57f7196
steambun <stephendewar@gmail.com>
2018-10-18 07:32:41 +0800

    redirected the path to the client directory

--- server/app.js
+++ server/app.js
@@ -23,10 +23,10 @@ app.use('/api', router)
 if (process.env.NODE_ENV === 'production') {
     console.log(`Server in production, redirecting all get requests to client`);
     // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static(path.join(__dirname, '../client/build')));
     // Handle React routing, return all requests to React app
     app.get('*', function(req, res) {
-      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+      res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
     });
   }
 
