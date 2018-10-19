c826f06d3236259da5316b3c34ae2b2e69a8c136
steambun <stephendewar@gmail.com>
2018-10-19 21:43:26 +0800

    Improved the server side dice logging

--- server/controllers/dice.ctrl.js
+++ server/controllers/dice.ctrl.js
@@ -4,11 +4,9 @@ module.exports ={
   rollDice: (req,res, next)=>{
     console.log(`Roll Dice Request Received`);
  
-    var dice1=Math.floor(Math.random() *(6)+1);
-    console.log(dice1);
-    
+    var dice1=Math.floor(Math.random() *(6)+1);    
     var dice2=Math.floor(Math.random() *(6)+1);
-    console.log(dice2);
+    console.log('Dice rolled ('+dice1+','+dice2+')');
     
     return res.json({"dice1":dice1,"dice2":dice2})
     
